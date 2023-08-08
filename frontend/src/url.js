dc97126ee267f12e2603595fb46ca1e0a1204733
Akshay2002Singh <06akshay2002@gmail.com>
2023-08-08 19:20:20 +0530

    updating url

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,6 +1,6 @@
 const obj = {
     'backend_url' : 'https://linktree-19kk.onrender.com',
-    'frontend_url' : 'http://localhost:3000'
+    'frontend_url' : 'https://linktree-frontend.onrender.com'
     // 'backend_url' : 'http://localhost:3000',
     // 'frontend_url' : 'http://localhost:3001'
 }
